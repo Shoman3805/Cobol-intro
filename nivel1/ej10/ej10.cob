9a8ebb107f7bb3564183dc72f787581faf556386
Román del Oso <133156480+Shoman3805@users.noreply.github.com>
2025-07-28 12:53:37 -0300

    Add files via upload

--- nivel1/ej10/ej10.cob
+++ nivel1/ej10/ej10.cob
@@ -7,11 +7,14 @@
        WORKING-STORAGE SECTION.
        01 ITERADOR PIC 9(2).
        01 TABLA PIC 9(2).
-       01 RES PIC 9(3).
+       01 RES PIC 9(4).
+       01 FIN PIC 9(2).
        PROCEDURE DIVISION.
            DISPLAY "Ingrese que tabla desea "
            ACCEPT TABLA
-           PERFORM UNTIL ITERADOR = 12
+           DISPLAY "Hasta donde"
+           ACCEPT FIN
+           PERFORM UNTIL ITERADOR = FIN
            ADD 1 TO ITERADOR
            MULTIPLY TABLA BY ITERADOR GIVING RES
            DISPLAY ITERADOR " X " TABLA " = " RES
